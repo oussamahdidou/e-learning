e16ca5a5d963d7a2a51cdf7dabb833a68b66381b
oussamahdidou <oussamahdidou15@gmail.com>
2024-07-03 17:41:28 +0100

    initialize project

---
+++ api/Data/apiDbContext.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class apiDbContext : IdentityDbContext<AppUser>
+    {
+        public apiDbContext(DbContextOptions dbContextOptions)
+        : base(dbContextOptions)
+        {
+
+        }
+        // public DbSet<Doctor> doctors { get; set; }
+        // public DbSet<LicenseDoctor> licenseDoctors { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // List<IdentityRole> Roles = new List<IdentityRole>()
+            // {
+            //     new IdentityRole()
+            //     {
+            //         Name="Admin",
+            //         NormalizedName="ADMIN"
+            //     },
+            //     new IdentityRole()
+            //     {
+            //         Name="User",
+            //         NormalizedName="User"
+            //     }
+            // };
+            // List<Country> countries = new List<Country>()
+            // {
+            //     new Country(){
+            //         Id=1,
+            //         Name="Morocco"
+            //     },
+            //     new Country(){
+            //         Id=2,
+            //         Name="France",
+            //     }
+            // };
+            // List<Ville> villes = new List<Ville>(){
+            //     new Ville(){
+            //         Id=1,
+            //         Name="Casablanca",
+            //         CountryId=1
+            //     },
+            //     new Ville(){
+            //         Id=2,
+            //         Name="Marrakech",
+            //         CountryId=1
+            //     },
+            //     new Ville(){
+            //          Id=3,
+            //         Name="Errachidia",
+            //         CountryId=1
+            //     },
+            // };
+
+            // builder.Entity<Rating>(x => x.HasKey(p => new { p.AppUserId, p.EntrepriseId }));
+            // builder.Entity<Rating>()
+            // .HasOne(u => u.AppUser)
+            // .WithMany(u => u.Ratings)
+            // .HasForeignKey(p => p.AppUserId);
+            // builder.Entity<Rating>()
+            // .HasOne(u => u.Entreprise)
+            // .WithMany(u => u.Ratings)
+            // .HasForeignKey(p => p.EntrepriseId);
+            // builder.Entity<Comment>(x =>
+            // {
+            //     x.HasKey(p => p.Id); // Set Id as the primary key
+            //     x.Property(p => p.Id).ValueGeneratedOnAdd(); // Configure Id to be auto-generated
+            //     x.HasOne(u => u.AppUser)
+            //         .WithMany(u => u.Comments)
+            //         .HasForeignKey(p => p.AppUserId);
+            //     x.HasOne(u => u.Entreprise)
+            //         .WithMany(u => u.Comments)
+            //         .HasForeignKey(p => p.EntrepriseId);
+            // });
+
+
+            // builder.Entity<IdentityRole>().HasData(Roles);
+            // builder.Entity<Country>().HasData(countries);
+            // builder.Entity<Ville>().HasData(villes);
+        }
+    }
+}
---
+++ api/Program.cs
@@ -0,0 +1,114 @@
+
+using api.Data;
+using api.Model;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers().AddNewtonsoftJson(
+    options =>
+    {
+        options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+
+    }
+);
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+builder.Services.AddDbContext<apiDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+});
+builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequiredLength = 8;
+
+})
+.AddEntityFrameworkStores<apiDbContext>();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme =
+    options.DefaultChallengeScheme =
+    options.DefaultForbidScheme =
+    options.DefaultScheme =
+    options.DefaultSignInScheme =
+    options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true, //issuer url same as jwt token creation url
+        ValidIssuer = builder.Configuration["JWT:Issuer"], //audience
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JWT:Audience"],//issuer url same as jwt token creation url
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(
+                System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigninKey"]))
+
+    };
+});
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigin",
+        builder => builder.WithOrigins("http://localhost:4200")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    );
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseStaticFiles();
+app.UseHttpsRedirection();
+app.UseCors("AllowOrigin");
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+
+
+app.Run();
+
