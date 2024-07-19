5baf88b4fedd3a300c5c3f8356a1e323241eac35
Ali Grich <128514098+grichali@users.noreply.github.com>
2024-07-19 19:15:46 +0100

    start working on courses

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './course/navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'course',
+    loadChildren: () =>
+      import('./course/course.module').then((m) => m.CourseModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/course/course.module.ts
+++ client/src/app/course/course.module.ts
@@ -1,12 +1,49 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+import { QuizComponent } from './quiz/quiz.component';
+import { LectureComponent } from './lecture/lecture.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { PdfViewerComponent } from './pdf-viewer/pdf-viewer.component';
+import { MatIconModule } from '@angular/material/icon';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { CourseComponent } from './course.component';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'navbar',
+        component: CourseComponent,
+      },
+      {
+        path: 'lecture',
+        component: LectureComponent,
+      },
+      {
+        path: 'quiz',
+        component: QuizComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent,
+    QuizComponent,
+    LectureComponent,
+    SidebarComponent,
+    PdfViewerComponent,
+    CourseComponent,
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
+    MatIconModule,
+    PdfViewerModule
   ]
 })
 export class CourseModule { }
