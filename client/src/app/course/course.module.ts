d814a1233a64924f0731ddc6b98ce267f902413c
youssef belkhiri <belkhiriyoussef33@gmail.com>
2024-07-19 19:21:05 +0100

    merge

--- client/src/app/course/course.module.ts
+++ client/src/app/course/course.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { QuizComponent } from './quiz/quiz.component';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'quiz',
+        component: QuizComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [QuizComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
 })
-export class CourseModule { }
+export class CourseModule {}
