74cfa833d3ef20dd5d82aac7774ed8963fe38e8c
oussamahdidou <oussamahdidou15@gmail.com>
2024-07-24 20:51:38 +0100

    create chapter create quiz upadte chapter dashboard angular

--- client/src/app/services/dashboard.service.ts
+++ client/src/app/services/dashboard.service.ts
@@ -9,61 +9,64 @@ import { environment } from '../../environments/environment';
 export class DashboardService {
   constructor(private readonly http: HttpClient) {}
   createinstitution(name: string): Observable<any> {
-    return this.http.post(`${environment.apiUrl}/Institution`, {
+    return this.http.post(`${environment.apiUrl}/api/Institution`, {
       name: name,
     });
   }
   getinstitutions(): Observable<any> {
-    return this.http.get(`${environment.apiUrl}/Institution`);
+    return this.http.get(`${environment.apiUrl}/api/Institution`);
   }
   updateinstitution(name: string, id: number): Observable<any> {
-    return this.http.put(`${environment.apiUrl}/Institution`, {
+    return this.http.put(`${environment.apiUrl}/api/Institution`, {
       id: id,
       nom: name,
     });
   }
   getniveauscolaires(id: number): Observable<any> {
-    return this.http.get(`${environment.apiUrl}/Institution/${id}`);
+    return this.http.get(`${environment.apiUrl}/api/Institution/${id}`);
   }
   createniveauscolaire(name: string, id: number): Observable<any> {
-    return this.http.post(`${environment.apiUrl}/NiveauScolaire`, {
+    return this.http.post(`${environment.apiUrl}/api/NiveauScolaire`, {
       nom: name,
       institutionId: id,
     });
   }
   updateniveauscolaire(name: string, id: number): Observable<any> {
-    return this.http.put(`${environment.apiUrl}/NiveauScolaire`, {
+    return this.http.put(`${environment.apiUrl}/api/NiveauScolaire`, {
       niveauScolaireId: id,
       nom: name,
     });
   }
   getModules(id: number): Observable<any> {
-    return this.http.get(`${environment.apiUrl}/NiveauScolaire/${id}`);
+    return this.http.get(`${environment.apiUrl}/api/NiveauScolaire/${id}`);
   }
   createModule(name: string, id: number): Observable<any> {
-    return this.http.post(`${environment.apiUrl}/Module`, {
+    return this.http.post(`${environment.apiUrl}/api/Module`, {
       nom: name,
       niveauScolaireId: id,
     });
   }
   updateModule(name: string, id: number): Observable<any> {
-    return this.http.put(`${environment.apiUrl}/Module`, {
+    return this.http.put(`${environment.apiUrl}/api/Module`, {
       nom: name,
       moduleId: id,
     });
   }
   createchapter(chapter: any): Observable<any> {
-    return this.http.post(`${environment.apiUrl}/Chapitre`, chapter);
+    return this.http.post(`${environment.apiUrl}/api/Chapitre`, chapter);
   }
   createquiz(quiz: any): Observable<any> {
-    return this.http.post(`${environment.apiUrl}/Quiz/Create`, quiz);
+    return this.http.post(`${environment.apiUrl}/api/Quiz/Create`, quiz);
   }
   getrequiredmodules(id: number): Observable<any> {
     return this.http.get(
-      `${environment.apiUrl}/ModuleRequirement/RequiredModules/${id}`
+      `${environment.apiUrl}/api/ModuleRequirement/RequiredModules/${id}`
     );
   }
   createmodulerequirements(module: any): Observable<any> {
-    return this.http.post(`${environment.apiUrl}/ModuleRequirement`, module);
+    return this.http.post(
+      `${environment.apiUrl}/api/ModuleRequirement`,
+      module
+    );
   }
 }
