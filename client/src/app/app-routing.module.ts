3c3461c183348291ef210692417111eb9654d444
oussamahdidou <oussamahdidou15@gmail.com>
2024-07-26 15:38:42 +0100

    push hdidou

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,20 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
-import { NiveauScolairesComponent } from './institutions/niveauscolaires/niveauscolaires.component';
-import { ModulesComponent } from './institutions/modules/modules.component';
-import { InstitutionsComponent } from './institutions/institutions/institutions.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
-import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
-import { VerifyEmailComponent } from './auth/verify-email/verify-email.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'forgot-password', component: ForgotPasswordComponent },
-  { path: 'reset-password', component: ResetPasswordComponent },
-  { path: 'verify-email', component: VerifyEmailComponent },
   {
     path: '',
     component: HomeComponent,
@@ -35,6 +22,11 @@ const routes: Routes = [
     path: 'dashboard',
     loadChildren: () =>
       import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+  },
+
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
   },
   {
     path: '**',
