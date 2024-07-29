fea9b3d340fac8633946a2e6206578122b44cff0
Ali Grich <128514098+grichali@users.noreply.github.com>
2024-07-29 21:36:40 +0100

    start implementing mylearning on profile part

--- client/src/app/profile/learning/learning.component.ts
+++ client/src/app/profile/learning/learning.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrl: './learning.component.css'
 })
 export class LearningComponent {
-
+  modules = [
+  {
+    moduleID: 1,
+    nom: "Mathematics",
+    numberOfChapter: 4,
+    checkCount: 2
+  },
+  {
+    moduleID: 3,
+    nom: "History",
+    numberOfChapter: 1,
+    checkCount: 1
+  },
+  {
+    moduleID: 3,
+    nom: "History",
+    numberOfChapter: 1,
+    checkCount: 1
+  },
+  {
+    moduleID: 3,
+    nom: "History",
+    numberOfChapter: 1,
+    checkCount: 1
+  },
+  {
+    moduleID: 3,
+    nom: "History",
+    numberOfChapter: 1,
+    checkCount: 1
+  },
+  {
+    moduleID: 3,
+    nom: "History",
+    numberOfChapter: 1,
+    checkCount: 1
+  }
+]
+  calculateProgress(moduleID : number){
+    const module = this.modules.find(x => x.moduleID == moduleID)
+    if(module){
+      return module.checkCount * 100 / module.numberOfChapter
+    }
+    return undefined
+  }
 }
