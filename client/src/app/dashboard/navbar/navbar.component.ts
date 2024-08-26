5a3d95359536ad5b2fba716b1c857aa67f3c1f8f
oussamahdidou <oussamahdidou15@gmail.com>
2024-08-26 13:48:32 +0100

    solving error  cours

--- client/src/app/dashboard/navbar/navbar.component.ts
+++ client/src/app/dashboard/navbar/navbar.component.ts
@@ -7,7 +7,7 @@ import { AuthService } from '../../services/auth.service';
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-  constructor(private readonly authservice: AuthService) {}
+  constructor(public readonly authservice: AuthService) {}
   logout() {
     this.authservice.logout();
   }
